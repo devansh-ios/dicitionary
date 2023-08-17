f7a8eafd854e63b8e779cb28cf4c11be673ecb33
devansh-ios <devansh.dev2592002@gmail.com>
2023-08-17 22:17:19 +0530

    first and last changes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react"; 
 
 function App() {
+  const [word, setWord] = useState([]);
+  const [searchTerm, setSearchTerm] = useState(""); 
+
+  const searchHandler = async () => {
+    const res = await fetch(
+      `https://api.dictionaryapi.dev/api/v2/entries/en/${searchTerm}` 
+    );
+    const data = await res.json();
+if(!res.ok){
+  alert(" word didn't found")
+}
+    setWord(data);
+    
+  };
+
+  const handleInputChange = (event) => {
+    setSearchTerm(event.target.value); 
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input
+        type="text"
+        placeholder="enter the word"
+        value={searchTerm}
+        onChange={handleInputChange} 
+      />
+      <br />
+      <button onClick={searchHandler}>Search</button>
+      <div>
+      {word.map((entry, index) => (
+        <div key={index}>
+          <h2>{entry.word}</h2>
+          {entry.meanings.map((meaning, meaningIndex) => (
+            <div key={meaningIndex}>
+              <h3>Part of Speech: {meaning.partOfSpeech}</h3>
+              <ul>
+                {meaning.definitions.map((definition, defIndex) => (
+                  <li key={defIndex}>{definition.definition}</li>
+                ))}
+              </ul>
+            </div>
+          ))}
+        </div>
+      ))}
     </div>
+      </div>
   );
 }
 
